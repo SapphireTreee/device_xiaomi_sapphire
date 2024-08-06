782444ae7c5606680526edfa57f8381853c57fec
AdarshGrewal <adarshgrewal@gmail.com>
2024-08-07 00:28:40 +0530

    sapphire: Build audioadsprpcd from source
    Change-Id: I19d38c534edf26886bd354548eaee661dd79f793
    Signed-off-by: Nikita Savchenko <NikitaSavc@yandex.ru>

--- device.mk
+++ device.mk
@@ -82,6 +82,9 @@ PRODUCT_PACKAGES += \
 
 AUDIO_HAL_DIR := hardware/qcom-caf/sm6225/audio/primary-hal
 
+PRODUCT_PACKAGES += \
+    audioadsprpcd
+
 PRODUCT_COPY_FILES += \
     $(AUDIO_HAL_DIR)/configs/common/audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy_configuration.xml \
     $(LOCAL_PATH)/configs/audio/audio_effects.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio/audio_effects.xml \
