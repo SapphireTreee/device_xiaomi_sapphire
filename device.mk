267ee59379562b3bbfda8f240680852c92f605f8
Arian <arian.kulmer@web.de>
2024-08-12 12:48:36 +0200

    sapphire: Kill libstagefrighthw
    This will not be loaded when vendor.media.omx=0 is set
    
    Change-Id: I6c87910028feae4c0711ce9bfc0ed2fb5e7445c0
    Signed-off-by: Nikita Savchenko <NikitaSavc@yandex.ru>

--- device.mk
+++ device.mk
@@ -424,11 +424,6 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.nfc.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.nfc.xml \
     frameworks/native/data/etc/android.hardware.se.omapi.uicc.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.se.omapi.uicc.xml
 
-# OMX
-PRODUCT_PACKAGES += \
-    libmm-omxcore \
-    libOmxCore
-
 # Overlays
 PRODUCT_ENFORCE_RRO_TARGETS := *
 
