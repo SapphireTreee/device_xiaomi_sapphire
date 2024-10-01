e1972dbd397d30922bd73e88ffc40381b234664e
SagarMakhar <sagarmakhar@gmail.com>
2024-10-01 23:59:30 +0530

    sapphire: Turn off persist.vendor.radio.poweron_opt
    It's set to 1 in qcril_database migrations. Sed the property
    to something else so it defaults to 0.
    
    When the value is 1 it breaks SMS receiving without QTI telephony.

--- extract-files.py
+++ extract-files.py
@@ -35,6 +35,10 @@ blob_fixups: blob_fixups_user_type = {
         .replace_needed('libstagefright_foundation.so', 'libstagefright_foundation-v33.so'),
     'libcodec2_hidl@1.0.so': blob_fixup()
         .add_needed('libshim.so'),
+    'vendor/etc/qcril_database/upgrade/config/6.0_config.sql': blob_fixup()
+        .binary_regex_replace(rb'persist\.vendor\.radio\.poweron_opt', rb'persist.vendor.radio.poweron_ign'),
+    ('vendor/lib64/libqcrilNr.so', 'vendor/lib64/libril-db.so'): blob_fixup()
+        .binary_regex_replace(rb'persist\.vendor\.radio\.poweron_opt', rb'persist.vendor.radio.poweron_ign'),
     'vendor/lib64/vendor.libdpmframework.so': blob_fixup()
         .add_needed('libhidlbase_shim.so'),
     'vendor/etc/seccomp_policy/c2audio.vendor.ext-arm64.policy': blob_fixup()
