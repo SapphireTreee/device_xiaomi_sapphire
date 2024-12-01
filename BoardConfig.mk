e15c42f2c084085d83f062edf591ca3b0e07d239
EmanuelCN <emanuelghub@gmail.com>
2024-12-01 22:43:34 +0000

    sapphire: Disable erofs compression

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -69,6 +69,9 @@ TARGET_USES_QCOM_MM_AUDIO := true
 # Boot control
 $(call soong_config_set, ufsbsg, ufsframework, bsg)
 
+# EROFS
+BOARD_EROFS_COMPRESSOR := none
+
 # Bootloader
 TARGET_BOOTLOADER_BOARD_NAME := bengal
 
