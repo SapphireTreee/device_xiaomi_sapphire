758d30106408bb58edf23b7d012a26668b26e426
Saroj Tajpuriya <80759612+saroj-nokia@users.noreply.github.com>
2025-02-01 16:52:14 +0545

    Revert "sapphire: Replace GCamGo as Default Camera"
    This reverts commit 9c81af99abd1c557565ed625441acb08b03ee741.

--- device.mk
+++ device.mk
@@ -435,10 +435,6 @@ PRODUCT_PACKAGES += \
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay-lineage
 
-# Packages
-PRODUCT_PACKAGES += \
-    GoogleCameraGo
-
 # Partitions
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
