88866ca101af0b7ca743717652b69e7fda7a0777
Saroj Tajpuriya <80759612+saroj-nokia@users.noreply.github.com>
2025-02-01 14:24:30 +0000

    sapphire: overlay: Configure aux camera for Aperture

--- device.mk
+++ device.mk
@@ -419,6 +419,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_ENFORCE_RRO_TARGETS := *
 
 PRODUCT_PACKAGES += \
+    ApertureResSapphire \
     CarrierConfigResCommon \
     FrameworksResCommon \
     FrameworksResTarget \
