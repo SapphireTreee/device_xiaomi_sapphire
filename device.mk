c0cb8cb6f4a5adb8f471a2c0938606b5436bc03b
Saroj Tajpuriya <80759612+saroj-nokia@users.noreply.github.com>
2025-02-05 09:08:35 +0000

    sapphire: device.mk: Update pre-optimized apps for launcher and SystemUI

--- device.mk
+++ device.mk
@@ -41,9 +41,9 @@ PRODUCT_DEX_PREOPT_BOOT_IMAGE_PROFILE_LOCATION := frameworks/base/config/boot-im
 
 # Dex
 PRODUCT_DEXPREOPT_SPEED_APPS += \
-    TrebuchetQuickStep \
+    NexusLauncherRelease \
     Settings \
-    SystemUI
+    SystemUIGoogle
 
 # ART
 ART_BUILD_TARGET_NDEBUG := true
