ee9d6dafcde9f7cef120011afc8b6f1514824139
Saroj Tajpuriya <80759612+saroj-nokia@users.noreply.github.com>
2025-02-26 11:40:13 +0545

    Revert "sapphire: Move to Xiaomi IR AIDL"
    This reverts commit 8439585bfd14b02f3ec1989d34a55c7a568a7393.

--- device.mk
+++ device.mk
@@ -299,7 +299,7 @@ PRODUCT_PACKAGES += \
 
 # IR
 PRODUCT_PACKAGES += \
-    android.hardware.ir-service.xiaomi
+    android.hardware.ir-V1-ndk.vendor
 
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.consumerir.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.consumerir.xml
