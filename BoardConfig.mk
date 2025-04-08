4d4b91781089a7c304e9a10981e3632ed421a7ae
Dyneteve <sound0020@gmail.com>
2025-04-08 05:08:51 +0000

    sapphire: Enable AOSP surfaceflinger
    - AOSP SurfaceFlinger is smoother than CAF
    
    Change-Id: Id671da2363fb09303a7d7241d5ae95e55ff16475
    Signed-off-by: RobertGarciaa <chae0218@naver.com>
    Signed-off-by: Omar Mohamed <omarcoptan9@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -212,6 +212,9 @@ include device/xiaomi/sepolicy/SEPolicy.mk
 SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/private
 BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
 
+# SurfaceFlinger
+TARGET_USE_AOSP_SURFACEFLINGER := true
+
 # Verified Boot
 BOARD_AVB_ENABLE := true
 BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --flags 3
