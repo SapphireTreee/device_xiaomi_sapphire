73816db1a28a6216152fb6f8cd3c9407d14a2f09
Saroj-Tajpuriya <80759612+saroj-nokia@users.noreply.github.com>
2025-05-02 03:46:25 +0000

    sapphire: parts: Remove unused import for DisplayManager in BootCompletedReceiver

--- parts/src/org/lineageos/settings/BootCompletedReceiver.java
+++ parts/src/org/lineageos/settings/BootCompletedReceiver.java
@@ -23,7 +23,6 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.database.ContentObserver;
-import android.hardware.display.DisplayManager;
 import android.os.Handler;
 import android.os.IBinder;
 import android.os.UserHandle;
