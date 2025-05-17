dadacf1c0b8da02da0dc6e44d04ea49d7c39b2d0
saroj-nokia <sarojtaj77@gmail.com>
2025-05-17 15:27:39 +0545

    Revert "sapphire: parts: Fix compilation error in BootCompletedReceiver for DozeUtils"
    This reverts commit b445f5bc4c6c583c79a6cd1a45598782b042685f.

--- parts/src/org/lineageos/settings/BootCompletedReceiver.java
+++ parts/src/org/lineageos/settings/BootCompletedReceiver.java
@@ -31,7 +31,6 @@ import android.util.Log;
 
 import org.lineageos.settings.thermal.ThermalUtils;
 import org.lineageos.settings.refreshrate.RefreshUtils;
-import org.lineageos.settings.doze.DozeUtils;
 
 public class BootCompletedReceiver extends BroadcastReceiver {
     private static final boolean DEBUG = false;
