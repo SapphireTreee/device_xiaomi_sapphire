7b7ddefd1bd6e0172f883510ae078ebdf2f17fb6
saroj-nokia <sarojtaj77@gmail.com>
2025-06-01 19:26:42 +0545

    Revert "sapphire: Add UDFPS support for Xiaomi sensors in device.mk"
    This reverts commit a0ce782a51ea41d219f766799ccb48536d88292c.

--- device.mk
+++ device.mk
@@ -535,10 +535,6 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/sensors/hals.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/hals.conf
 
 # Sensors
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.vendor.sensors.xiaomi.udfps=true
-
-# Sensors
 PRODUCT_PACKAGES += \
     android.frameworks.sensorservice@1.0.vendor \
     android.hardware.sensors-service.xiaomi-multihal \
