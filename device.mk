4916ab3678a08057dbb579ad83858d87e35f2ca4
Saroj-Tajpuriya <80759612+saroj-nokia@users.noreply.github.com>
2025-06-05 09:23:40 +0545

    sapphire: overlay: update telephony injection jar paths for consistency

--- device.mk
+++ device.mk
@@ -571,6 +571,8 @@ PRODUCT_PACKAGES += \
     extphonelib_product.xml \
     ims-ext-common \
     ims_ext_common.xml \
+    qti-telephony-common \
+    qti-telephony-common.xml \
     qti-telephony-hidl-wrapper \
     qti-telephony-hidl-wrapper-prd \
     qti_telephony_hidl_wrapper.xml \
