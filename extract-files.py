cf4e726a640c5a815544486f555dcff083f40cca
saroj-nokia <sarojtaj77@gmail.com>
2025-06-29 10:17:03 +0545

    sapphire: Switch to python extract-utils
    Co-authored-by: LuK1337 <priv.luk@gmail.com>
    Co-authored-by: Arian <arian.kulmer@web.de>
    Co-authored-by: SGCMarkus <markusornik@gmail.com>

---
+++ extract-files.py
@@ -0,0 +1,76 @@
+#!/usr/bin/env -S PYTHONPATH=../../../tools/extract-utils python3
+#
+# SPDX-FileCopyrightText: 2024 The LineageOS Project
+# SPDX-License-Identifier: Apache-2.0
+#
+
+from extract_utils.fixups_blob import (
+    blob_fixup,
+    blob_fixups_user_type,
+)
+
+from extract_utils.fixups_lib import (
+    lib_fixup_remove,
+    lib_fixups,
+    lib_fixups_user_type,
+)
+
+from extract_utils.main import (
+    ExtractUtils,
+    ExtractUtilsModule,
+)
+
+namespace_imports = [
+    'device/xiaomi/sapphire',
+    'hardware/qcom-caf/sm6225',
+    'hardware/qcom-caf/wlan',
+    'hardware/xiaomi',
+    'vendor/qcom/opensource/commonsys-intf/display',
+    'vendor/qcom/opensource/dataservices',
+    'vendor/qcom/opensource/display',
+]
+
+
+def lib_fixup_vendor_suffix(lib: str, partition: str, *args, **kwargs): # fmt: skip
+    return f'{lib}_{partition}' if partition == 'vendor' else None # fmt: skip
+
+
+lib_fixups: lib_fixups_user_type = {
+    **lib_fixups,
+}
+
+blob_fixups: blob_fixups_user_type = {
+    ('vendor/bin/hw/android.hardware.security.keymint-service-qti', 'vendor/lib64/libqtikeymint.so'): blob_fixup()
+        .add_needed('android.hardware.security.rkp-V3-ndk.so'),
+    ('vendor/lib64/hw/displayfeature.default.so'): blob_fixup()
+        .replace_needed('libstagefright_foundation.so', 'libstagefright_foundation-v33.so'),
+    'libcodec2_hidl@1.0.so': blob_fixup()
+        .add_needed('libshim.so'),
+    'vendor/lib64/vendor.libdpmframework.so': blob_fixup()
+        .add_needed('libhidlbase_shim.so'),
+    'vendor/etc/seccomp_policy/c2audio.vendor.ext-arm64.policy': blob_fixup()
+        .add_line_if_missing('setsockopt: 1'),
+   ('vendor/lib64/hw/com.qti.chi.override.so',
+    'vendor/lib64/libcamxcommonutils.so',
+    'vendor/lib64/libmialgoengine.so',
+    'vendor/lib64/hw/camera.qcom.so',): blob_fixup()
+        .add_needed('libprocessgroup_shim.so'),
+    'vendor/etc/init/android.hardware.gnss-aidl-service-qti.rc': blob_fixup()
+        .regex_replace('group system gps radio vendor_qti_diag vendor_ssgtzd', 'group system gps radio vendor_qti_diag'),
+    'vendor/bin/STFlashTool': blob_fixup()
+        .add_needed('libbase_shim.so'),
+    'vendor/lib64/libwvhidl.so': blob_fixup()
+        .add_needed('libcrypto_shim.so')
+}  # fmt: skip
+
+module = ExtractUtilsModule(
+    'sapphire',
+    'xiaomi',
+    blob_fixups=blob_fixups,
+    lib_fixups=lib_fixups,
+    namespace_imports=namespace_imports,
+)
+
+if (__name__ == '__main__'): # fmt: skip
+    utils = ExtractUtils.device(module) # fmt: skip
+    utils.run()
