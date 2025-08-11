8502fde49fe3e466269364ec6a3fb007f51d3449
Saroj-Tajpuriya <80759612+saroj-nokia@users.noreply.github.com>
2025-08-11 14:27:03 +0545

    sapphire: drop some files
    let switch to kernel default for this

--- device.mk
+++ device.mk
@@ -314,7 +314,6 @@ PRODUCT_PACKAGES += \
     init.qcom.rc \
     init.qti.kernel.rc \
     init.recovery.qcom.rc \
-    init.sapphire.perf.rc \
     init.target.rc \
     init.xiaomi.rc \
     ueventd.qcom.rc
@@ -332,7 +331,6 @@ PRODUCT_PACKAGES += \
     init.qti.early_init.sh \
     init.qti.kernel.sh \
     init.qti.write.sh \
-    init.sapphire_perf.sh \
     system_dlkm_modprobe.sh \
     vendor_modprobe.sh
 
