f972c5d9aa747a6224b19c6512f64286f871551b
Saroj-Tajpuriya <80759612+saroj-nokia@users.noreply.github.com>
2025-08-12 23:28:37 +0545

    Revert "sapphire: drop some files"
    This reverts commit 8502fde49fe3e466269364ec6a3fb007f51d3449.

--- device.mk
+++ device.mk
@@ -314,6 +314,7 @@ PRODUCT_PACKAGES += \
     init.qcom.rc \
     init.qti.kernel.rc \
     init.recovery.qcom.rc \
+    init.sapphire.perf.rc \
     init.target.rc \
     init.xiaomi.rc \
     ueventd.qcom.rc
@@ -331,6 +332,7 @@ PRODUCT_PACKAGES += \
     init.qti.early_init.sh \
     init.qti.kernel.sh \
     init.qti.write.sh \
+    init.sapphire_perf.sh \
     system_dlkm_modprobe.sh \
     vendor_modprobe.sh
 
