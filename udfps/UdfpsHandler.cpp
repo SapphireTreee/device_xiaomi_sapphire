0d5b350dc23aa8903e47753e698badc47504e7dd
saroj-nokia <sarojtaj77@gmail.com>
2025-09-05 13:46:30 +0545

    sapphire: udfps: Update FOD status handling for improved reliability

--- udfps/UdfpsHandler.cpp
+++ udfps/UdfpsHandler.cpp
@@ -221,7 +221,7 @@ class XiaomiSm6225UdfpsHandler : public UdfpsHandler {
          * The finger down message is only reliably sent when the screen is turned off, so enable
          * fod_status better late than never.
          */
-        if (vendorCode == 21 || vendorCode == 22) {
+        if (!isFpcFod || vendorCode == 21 || vendorCode == 22) {
             setFodStatus(FOD_STATUS_ON);
         }
     }
