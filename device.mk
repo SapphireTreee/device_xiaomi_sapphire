5fd3233ed0173a9aa6539d616978bdd6e9511b75
saroj-nokia <sarojtaj77@gmail.com>
2025-12-21 15:27:57 +0545

    Revert "sapphire; some experimental changes"
    This reverts commit 407a8b8fc4f83979c5f67f7783d04c3aae1b5e97.

--- device.mk
+++ device.mk
@@ -319,6 +319,7 @@ PRODUCT_PACKAGES += \
     init.qcom.rc \
     init.qti.kernel.rc \
     init.recovery.qcom.rc \
+    init.sapphire.perf.rc \
     init.target.rc \
     init.xiaomi.rc \
     ueventd.qcom.rc
@@ -337,6 +338,7 @@ PRODUCT_PACKAGES += \
     init.qti.early_init.sh \
     init.qti.kernel.sh \
     init.qti.write.sh \
+    init.sapphire_perf.sh \
     system_dlkm_modprobe.sh \
     vendor_modprobe.sh
 
