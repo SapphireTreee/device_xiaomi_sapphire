121e50cd48f54c530bcebbfd4065852683dfb719
saroj-nokia <sarojtaj77@gmail.com>
2025-12-23 12:22:42 +0545

    sapphire: remve custom fingerprint to test

--- lineage_sapphire.mk
+++ lineage_sapphire.mk
@@ -34,7 +34,3 @@ PRODUCT_BRAND := Redmi
 PRODUCT_MODEL := Redmi Note 13
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
-
-PRODUCT_BUILD_PROP_OVERRIDES += \
-    BuildDesc="sapphire_global-user 15 AQ3A.240829.003 OS2.0.207.0.VNGMIXM release-keys" \
-    BuildFingerprint=Redmi/sapphire_global/sapphire:15/AQ3A.240829.003/OS2.0.207.0.VNGMIXM:user/release-keys
