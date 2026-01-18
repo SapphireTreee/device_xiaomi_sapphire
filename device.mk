51be21cc081b0eb9603914e91082450f7437ebc9
saroj-nokia <sarojtaj77@gmail.com>
2026-01-18 21:49:06 +0545

    Revert "sapphire; : Enable Updateable APEX"
    This reverts commit 7a8a2d509c913bf3a68ae37cdc2f3f5b8b59c424.

--- device.mk
+++ device.mk
@@ -7,9 +7,6 @@
 # Add common definitions for Qualcomm
 $(call inherit-product, hardware/qcom-caf/common/common.mk)
 
-# APEX
-$(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
-
 # Enable project quotas and casefolding for emulated storage without sdcardfs
 $(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
 
