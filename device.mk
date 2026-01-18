7a8a2d509c913bf3a68ae37cdc2f3f5b8b59c424
saroj-nokia <sarojtaj77@gmail.com>
2026-01-18 21:21:58 +0545

    sapphire; : Enable Updateable APEX

--- device.mk
+++ device.mk
@@ -7,6 +7,9 @@
 # Add common definitions for Qualcomm
 $(call inherit-product, hardware/qcom-caf/common/common.mk)
 
+# APEX
+$(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
+
 # Enable project quotas and casefolding for emulated storage without sdcardfs
 $(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
 
