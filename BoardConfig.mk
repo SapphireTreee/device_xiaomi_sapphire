e3e76509cf0859577ad9d096feeccc6d8e3c2fad
saroj-nokia <sarojtaj77@gmail.com>
2026-03-23 21:49:11 +0545

    Revert "sapphire: Use hwui and add some hwui props for improve perf"
    This reverts commit b67109a5ebd3f6dfc8957905bdfd3288b0e4fdc6.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -86,9 +86,6 @@ TARGET_PD_SERVICE_ENABLED := true
 #Enable peripheral manager
 TARGET_PER_MGR_ENABLED := true
 
-# HWUI
-HWUI_COMPILE_FOR_PERF := true
-
 # Init
 $(call soong_config_set,libinit,vendor_init_lib,//$(DEVICE_PATH):init_sapphire)
 
