bfdffa4fc7864466ae1eb4f57e5c7d4287751bd9
saroj-nokia <sarojtaj77@gmail.com>
2026-03-28 13:54:19 +0545

    sapphire: remove skip_speaker_layout_channel_mask_field configuration from audio settings

--- device.mk
+++ device.mk
@@ -61,7 +61,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # Audio
 $(call soong_config_set, android_hardware_audio, run_64bit, true)
-$(call soong_config_set, android_hardware_audio, skip_speaker_layout_channel_mask_field, true)
 
 PRODUCT_PACKAGES += \
     android.hardware.audio@7.0-impl \
