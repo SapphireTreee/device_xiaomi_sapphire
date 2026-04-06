08b773652a7ec2e5c5c55a431741d31ac0d8b863
saroj-nokia <sarojtaj77@gmail.com>
2026-04-06 12:16:57 +0545

    sapphire: implement library suffix fixups and update namespace imports for proprietary binaries

--- extract-files.py
+++ extract-files.py
@@ -4,11 +4,15 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-from extract_utils.file import File
 from extract_utils.fixups_blob import (
-    BlobFixupCtx,
     blob_fixup,
     blob_fixups_user_type,
+)
+
+from extract_utils.fixups_lib import (
+    lib_fixup_remove,
+    lib_fixups,
+    lib_fixups_user_type,
 )
 
 from extract_utils.main import (
@@ -16,17 +20,35 @@ from extract_utils.main import (
     ExtractUtilsModule,
 )
 
-def blob_fixup_test_flag(
-    ctx: BlobFixupCtx,
-    file: File,
-    file_path: str,
-    *args,
-    **kargs,
-):
-    with open(file_path, 'rb+') as f:
-        f.seek(1337)
-        f.write(b'\x01')
+namespace_imports = [
+    'device/xiaomi/sapphire',
+    'hardware/qcom-caf/sm6225',
+    'hardware/qcom-caf/wlan',
+    'hardware/xiaomi',
+    'vendor/qcom/opensource/commonsys-intf/display',
+    'vendor/qcom/opensource/dataservices',
+    'vendor/qcom/opensource/display',
+]
 
+
+def lib_fixup_vendor_suffix(lib: str, partition: str, *args, **kwargs): # fmt: skip
+    return f'{lib}_{partition}' if partition == 'vendor' else None # fmt: skip
+
+
+lib_fixups: lib_fixups_user_type = {
+    **lib_fixups,    
+    (
+        'com.qualcomm.qti.dpm.api@1.0',
+        'vendor.qti.diaghal@1.0',
+        'vendor.qti.hardware.fm@1.0',
+        'vendor.qti.hardware.qccsyshal@1.0',
+        'vendor.qti.hardware.qccsyshal@1.1',
+        'vendor.qti.hardware.qccsyshal@1.2',
+        'vendor.qti.hardware.qccvndhal@1.0',
+        'vendor.qti.imsrtpservice@3.0',
+        'vendor.qti.imsrtpservice@3.1',
+    ): lib_fixup_vendor_suffix,
+}
 
 blob_fixups: blob_fixups_user_type = {
     ('vendor/bin/hw/android.hardware.security.keymint-service-qti', 'vendor/lib64/libqtikeymint.so'): blob_fixup()
@@ -63,6 +85,8 @@ module = ExtractUtilsModule(
     'xiaomi',
     blob_fixups=blob_fixups,
     check_elf=False,
+    lib_fixups=lib_fixups,
+    namespace_imports=namespace_imports,
 )
 
 if __name__ == '__main__':
