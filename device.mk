833e38adfc71df11de6e97311ece41409b5fe6de
saroj-nokia <sarojtaj77@gmail.com>
2026-05-13 16:53:56 +0545

    sapphire: Remove XiaomiVoIPFix service and related files

--- device.mk
+++ device.mk
@@ -445,10 +445,6 @@ PRODUCT_PACKAGES += \
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay-lineage
 
-# Device-specific settings
- PRODUCT_PACKAGES += \
-    XiaomiVoIPFix
-
 # Partitions
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
