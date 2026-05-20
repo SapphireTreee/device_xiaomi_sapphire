aa5a49d100a5cf674bed82731237cf6fb6436b10
saroj-nokia <sarojtaj77@gmail.com>
2026-05-20 18:42:05 +0545

    Revert "sapphire: Update cpuset parameters for background and system-background tasks"
    This reverts commit 6f22d0b7b7a00435af14615957b03d84b9bc1533.

--- rootdir/bin/init.kernel.post_boot-bengal.sh
+++ rootdir/bin/init.kernel.post_boot-bengal.sh
@@ -273,8 +273,8 @@ echo $silver_early_upmigrate $gold_early_upmigrate > /proc/sys/walt/sched_early_
 echo 325 > /proc/sys/walt/walt_low_latency_task_threshold
 
 # cpuset parameters
-echo 0-1 > /dev/cpuset/background/cpus
-echo 0-2 > /dev/cpuset/system-background/cpus
+echo 0-3 > /dev/cpuset/background/cpus
+echo 0-3 > /dev/cpuset/system-background/cpus
 
 # Turn off scheduler boost at the end
 echo 0 > /proc/sys/walt/sched_boost
