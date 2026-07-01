4045b22508f5ce33f2e17a86f6b6174a8f605d06
saroj-nokia <sarojtaj77@gmail.com>
2026-07-01 09:32:44 +0545

    Remove unnecessary protobuf-related entries from extract-files and proprietary-files

--- extract-files.py
+++ extract-files.py
@@ -73,10 +73,6 @@ blob_fixups: blob_fixups_user_type = {
         .add_needed('libbase_shim.so'),
     'vendor/lib64/libwvhidl.so': blob_fixup()
         .add_needed('libcrypto_shim.so'),
-    'system_ext/lib64/vendor.qti.hardware.qccsyshal@1.2-halimpl.so': blob_fixup()
-        .replace_needed('libprotobuf-cpp-full.so', 'libprotobuf-cpp-full-fromvendor.so'),
-    'system_ext/lib64/libprotobuf-cpp-full-fromvendor.so': blob_fixup()
-        .replace_needed('libprotobuf-cpp-lite.so','libprotobuf-cpp-lite-fromvendor.so'),
     'vendor/etc/init/hw/init.qcom.usb.rc': blob_fixup()
         .regex_replace('on charger', 'on property:init.svc.vendor.charger=running'),
 }
